9685e45ec6ff2a8527dd24fb6f73486ec5d4e0f1
umedjon01 <najottalimfn15@gmail.com>
2024-01-20 10:18:50 +0500

    HOMEWORK

---
+++ index.js
@@ -0,0 +1,98 @@
+//  1-chi masala
+
+// const a = 1;
+
+// for (let i = a; i < 101; i++) {
+//   console.log(i);
+// }
+
+//  2-chi masala
+// Foydalanuvchi istalgan sonni yani "n" kiritsa  algoritm 1 dan n gacha bo'lgan sonlarni ekranga chiqarib beradi.
+
+// let n = 87; //+prompt("Son kiriting")
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+//  3-chi masala
+
+// let n = 9; //+prompt("Son kiriting")
+
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// 4-chi misol
+
+// let n = 9; //+prompt("Son kiriting")
+
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 != 0) {
+//     sum = sum + i;
+//     //console.log(i);
+//   }
+// }
+// console.log(sum);
+
+//   5-chi misol
+
+// let b = 6; //+prompt("Son kiriting")
+
+// let sum = 1;
+
+// for (let i = 1; i <= b; i++) {
+//   if (i % 2 == 0) {
+//     sum = sum * i;
+//     // console.log(i);
+//   }
+// }
+// console.log(sum);
+
+//   6-chi masala
+
+// let b = 70;
+
+// let sum = 0;
+
+// for (let i = 1; i <= b; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     sum = sum + i;
+//     // console.log(i);
+//   }
+// }
+// console.log(sum);
+
+// 7 chi masala
+
+// let son = 8;
+// let b = 0;
+// for (let i = 1; i <= son; i++) {
+//   if (son % i == 0) {
+//     b++;
+//     console.log(i);
+//   }
+// }
+// console.log(
+//   "Bu kiritilgan   " + son + "  soninig   " + "Buluvchilar soni - " + b + " ta"
+// );
+
+//  8-chi masala
+
+// let son = 11;
+// let natija = "";
+
+// if (son <= 0) {
+//   console.log("tub son emas");
+// } else if (son == 1 || son == 2 || son == 3 || son == 5 || son == 7) {
+//   console.log("Tub son");
+// } else if (son % 2 == 0 || son % 3 == 0 || son % 5 == 0 || son % 7 == 0) {
+//   console.log("tub son emas");
+// } else {
+//   console.log("Tub son");
+// }
